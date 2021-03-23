8abac7324657955c8049e9fa05905917c356d96e
Piyamin <piyaminjaima42@gmail.com>
2021-03-23 16:03:03 +0700

    Graph

--- resources/views/Graph.blade.php
+++ resources/views/Graph.blade.php
@@ -52,35 +52,47 @@
             <div class="page-header">
                 <h3 class="newFont"> กราฟตัวชี้วัด </h3>
             </div>
-            <div id="chart">
+            <!-- chart-year -->
+            <div class="row">
+              <div class="col">
+                <div id="chart-year">
+                </div>
+              </div>
             </div>
-
-
+            <!-- chart-quarter -->
+            <!-- <div class="row">
+              <div class="col">
+                <div id="chart-quarter">
+                </div>
+              </div>
+            </div> -->
+            <!-- chart-month -->
+            <div class="row">
+              <div class="col">
+                <div id="chart-month">
+                </div>
+              </div>
+            </div>
         </div>
         @include('partials.footer')
     </div>
-    <!-- Div nav & side -->
-    <!-- </div> -->
-    <!-- </div>
-    </div> -->
-
-</body>
-
+  </body>
+<!-- chart-year -->
 <script>
     $('select').selectpicker();
 
-    
-
-    var options = {
+    var optionsYear = {
           series: [{
-          name: 'Net Profit',
-          data: [44, 55, 57, 56, 61, 58, 63, 60, 66]
-        }, {
-          name: 'Revenue',
-          data: [76, 85, 101, 98, 87, 105, 91, 114, 94]
-        }, {
-          name: 'Free Cash Flow',
-          data: [35, 41, 36, 26, 45, 48, 52, 53, 41]
+          name: 'Q1',
+          data: [30,40,50]
+        },
+        {
+          name: 'Q2',
+          data: [54,85,40]
+        },
+        {
+          name: 'Q3',
+          data: [43,63,50]
         }],
           chart: {
           type: 'bar',
@@ -102,26 +114,99 @@
           colors: ['transparent']
         },
         xaxis: {
-          categories: ['Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct'],
+          categories: ['2562','2563','2564'],
         },
-        yaxis: {
-          title: {
-            text: '$ (thousands)'
-          }
-        },
+        // yaxis: {
+        //   title: {
+        //     text: '$ (thousands)'
+        //   }
+        // },
         fill: {
           opacity: 1
         },
         tooltip: {
           y: {
             formatter: function (val) {
-              return "$ " + val + " thousands"
+              return val
             }
           }
         }
         };
 
-        var chart = new ApexCharts(document.querySelector("#chart"), options);
+        var chart = new ApexCharts(document.querySelector("#chart-year"), optionsYear);
         chart.render();
+</script>
+<!-- chart-quarter -->
+<script>
+    var optionsQuarter = {
+          series: [{
+          name: 'Net Profit',
+          data: [30, 55, 67]
+        },],
+          chart: {
+          type: 'bar',
+          height: 350
+        },
+        plotOptions: {
+          bar: {
+            horizontal: false,
+            columnWidth: '55%',
+            endingShape: 'rounded'
+          },
+        },
+        dataLabels: {
+          enabled: false
+        },
+        stroke: {
+          show: true,
+          width: 2,
+          colors: ['transparent']
+        },
+        xaxis: {
+          categories: ['Q1','Q2','Q3'],
+        },
+        fill: {
+          opacity: 1
+        },
+    };
+        var chartQuarter = new ApexCharts(document.querySelector("#chart-quarter"), optionsQuarter);
+        chartQuarter.render();
+
+</script>
+<!-- chart-month -->
+<script>
+    var optionsMonth = {
+          series: [{
+          name: 'Value',
+          data: [44, 55, 57, 56, 61, 58, 63, 60, 66,47,58,62]
+        },],
+          chart: {
+          type: 'bar',
+          height: 350
+        },
+        plotOptions: {
+          bar: {
+            horizontal: false,
+            columnWidth: '55%',
+            endingShape: 'rounded'
+          },
+        },
+        dataLabels: {
+          enabled: false
+        },
+        stroke: {
+          show: true,
+          width: 2,
+          colors: ['transparent']
+        },
+        xaxis: {
+          categories: ['Oct','Nov','Dec','Jan','Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep'],
+        },
+        fill: {
+          opacity: 1
+        },
+    };
+        var chartQuarter = new ApexCharts(document.querySelector("#chart-month"), optionsMonth);
+        chartQuarter.render();
 
 </script>
