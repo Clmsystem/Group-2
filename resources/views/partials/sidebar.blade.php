f69a73a23c05a92a87aec91d7577b123d8ff73d8
wic0144 <arm_ruttanard@hotmail.com>
2021-03-23 16:20:12 +0700

    fix login

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -10,7 +10,7 @@
           </div>
           <div class="nav-profile-text d-flex flex-column">
             <span class="font-weight-bold mb-2">{{session()->get('user')['name_employee']}}</span>
-            <span class="text-secondary text-small">{{session()->get('user')['name_position']}}<  /span>
+            <span class="text-secondary text-small">{{session()->get('user')['name_position']}}</span>
           </div>
           <i class="mdi mdi-bookmark-check text-success nav-profile-badge"></i>
         </a>
